7181a8b456d5e79cbd156f08c02e4f98560bcf3a
cotpetru24 <cotpetru1@gmail.com>
2024-07-06 23:53:41 +0100

    .env and .gitignore added

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
-console.log('welcome to willcraft server')
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+
+
+const app = express();
+
+app.listen(port, () => console.log(`server is listening on port ${port}`))
