76f4aa12f7bcd9a9a542493412905e0a7b713c73
cotpetru24 <cotpetru1@gmail.com>
2024-08-29 23:31:05 +0100

    adde the rate us lint to order list item

--- frontend/src/components/OrdersList/OrderItem.js
+++ frontend/src/components/OrdersList/OrderItem.js
@@ -1,7 +1,7 @@
 import { useDispatch } from "react-redux";
 import { deleteOrder } from "../../features/orders/ordersSlice";
 import { getOrderThunk } from "../../features/currentOrder/currentOrderSlice";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { resetOrderState } from "../../utils/reduxUtils";
 import { Container } from "react-bootstrap";
 import { Col, Row } from "react-bootstrap";
@@ -190,6 +190,25 @@ const OrderItem = ({ order }) => {
                                 )}
                             </Col>
                         </Row>
+
+
+
+
+                        {order.status === "complete" &&
+                            (
+                                <Row >
+                                    <Col className="d-flex justify-content-end">
+                                        <Link to="/writeAReview" style={{ textDecoration: 'none', fontSize:'12px' }} 
+                                          onClick={() => {
+                                            window.scrollTo(0, 0);
+                                          }}
+                                        >
+                                            Tell us how we did
+                                        </Link>
+                                    </Col>
+                                </Row>
+                            )
+                        }
                     </div>
                 </Card.Body>
             </Card>
